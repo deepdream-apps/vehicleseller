9753bdd54509ae04afe36adf639f4728e3e9d9ab
deepdream-apps <silvere.djam@gmail.com>
2021-09-06 03:23:39 +0200

    Backend

--- vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/service/UserService.java
+++ vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/service/UserService.java
@@ -8,7 +8,6 @@ import cm.deepdream.vehicleseller.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 public class UserService {
 	@Autowired
 	private UserRepository userRepository ;
