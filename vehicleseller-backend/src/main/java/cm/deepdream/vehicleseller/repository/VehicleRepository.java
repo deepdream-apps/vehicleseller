b085f284c0ad54b6817223344e4c1c39145e86f2
deepdream-apps <silvere.djam@gmail.com>
2021-09-06 03:44:08 +0200

    Backend update

--- vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/repository/VehicleRepository.java
+++ vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/repository/VehicleRepository.java
@@ -7,16 +7,5 @@ import cm.deepdream.vehicleseller.model.Model;
 import cm.deepdream.vehicleseller.model.Vehicle;
 @Repository
 public interface VehicleRepository extends CrudRepository<Vehicle, Long>{
-	public List<Vehicle> findByBrandAndFuelAndYearBetweenAndMileageBetweenAndPriceBetween(Brand brand, String fuel, 
-			Long yearFrom, Long yearTo, Long mileageMin, Long mileageMax, Long priceMin, Long priceMax) ;
-	public List<Vehicle> findByModelAndFuelAndYearBetweenAndMileageBetweenAndPriceBetween(Model model, String fuel, 
-			Long yearFrom, Long yearTo, Long mileageMin, Long mileageMax, Long priceMin, Long priceMax) ;
-	public List<Vehicle> findByFuelAndYearBetweenAndMileageBetweenAndPriceBetween(String fuel, Long yearFrom, Long yearTo, Long mileageMin, Long mileageMax, 
-			Long priceMin, Long priceMax) ;
-	public List<Vehicle> findByBrandAndYearBetweenAndMileageBetweenAndPriceBetween(Brand brand, Long yearFrom, 
-			Long yearTo, Long mileageMin, Long mileageMax, Long priceMin, Long priceMax) ;
-	public List<Vehicle> findByModelAndYearBetweenAndMileageBetweenAndPriceBetween(Model model, Long yearFrom, 
-			Long yearTo, Long mileageMin, Long mileageMax, Long priceMin, Long priceMax) ;
-	public List<Vehicle> findByYearBetweenAndMileageBetweenAndPriceBetween(Long yearFrom, Long yearTo, Long mileageMin, Long mileageMax, 
-			Long priceMin, Long priceMax) ;
+	
 }
