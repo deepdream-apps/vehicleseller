287110f2e730617ff69eb9cb691aebbabcdbde3a
deepdream-apps <silvere.djam@gmail.com>
2021-09-06 04:07:12 +0200

    Push CustomerRepository

---
+++ vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package cm.deepdream.vehicleseller.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import cm.deepdream.vehicleseller.model.Customer;
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+}
