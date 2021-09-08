d5d44138ad7248f5327f68a9935411ac79b42e44
deepdream-apps <silvere.djam@gmail.com>
2021-09-08 02:54:58 +0200

    Add test application properties and Test class BrandWebServiceTest

--- vehicleseller-backend/src/test/java/cm/deepdream/vehicleseller/service/BrandWebServiceTest.java
+++ vehicleseller-backend/src/test/java/cm/deepdream/vehicleseller/service/BrandWebServiceTest.java
@@ -1,8 +1,16 @@
 package cm.deepdream.vehicleseller.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
+@SpringBootTest
 public class BrandWebServiceTest {
+	@Value("${server.port}")
+	private int serverPort ;
 	
-	
+	@BeforeEach
 	public void setUp() {
 		
 	}
