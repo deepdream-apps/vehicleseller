63f19f34321cf80e7441888a53f16a119756cada
deepdream-apps <silvere.djam@gmail.com>
2021-09-08 18:28:42 +0200

    Correct docker config in pom.xml

--- vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/config/VehicleSellerTestConfig.java
+++ vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/config/VehicleSellerTestConfig.java
@@ -1,12 +1,10 @@
 package cm.deepdream.vehicleseller.config;
 import javax.sql.DataSource;
-
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-
+@TestConfiguration
 public class VehicleSellerTestConfig {
 	
 	@Profile("test")
--- vehicleseller-backend/src/test/java/cm/deepdream/vehicleseller/service/BrandWebServiceTest.java
+++ vehicleseller-backend/src/test/java/cm/deepdream/vehicleseller/service/BrandWebServiceTest.java
@@ -3,8 +3,12 @@ package cm.deepdream.vehicleseller.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import cm.deepdream.vehicleseller.config.VehicleSellerTestConfig;
 @ActiveProfiles("test")
+@Import(VehicleSellerTestConfig.class)
 @SpringBootTest
 public class BrandWebServiceTest {
 	@Value("${server.port}")
