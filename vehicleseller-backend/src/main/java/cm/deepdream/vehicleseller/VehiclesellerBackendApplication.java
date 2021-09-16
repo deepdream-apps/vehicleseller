8c1a76d35b6221ba514e26a3601770a6085ceaba
deepdream-apps <silvere.djam@gmail.com>
2021-09-16 08:34:25 +0200

    Encrypt all credentials information from all properties file

--- vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/VehiclesellerBackendApplication.java
+++ vehicleseller-backend/src/main/java/cm/deepdream/vehicleseller/VehiclesellerBackendApplication.java
@@ -35,6 +35,7 @@ public class VehiclesellerBackendApplication {
 
 
 	public static void main(String[] args) {
+		System.setProperty("jasypt.encryptor.password", "password");
 		SpringApplication.run(VehiclesellerBackendApplication.class, args);
 	}
 	
