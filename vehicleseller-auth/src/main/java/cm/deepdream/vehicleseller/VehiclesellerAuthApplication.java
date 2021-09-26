ed41d1089718162650934a88f08cb1ad445fd955
deepdream-apps <silvere.djam@gmail.com>
2021-09-27 00:58:33 +0200

    # WARNING: head commit changed in the meantime
    Add tests config and sec

---
+++ vehicleseller-auth/src/main/java/cm/deepdream/vehicleseller/VehiclesellerAuthApplication.java
@@ -0,0 +1,13 @@
+package cm.deepdream.vehicleseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesellerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesellerAuthApplication.class, args);
+	}
+
+}
