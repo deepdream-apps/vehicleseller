40ed6b4239f866393abfe2a57415971bfacc6231
deepdream-apps <silvere.djam@gmail.com>
2021-10-02 22:34:04 +0200

    Add vehicleseller-backend-data

---
+++ vehicleseller-backend-data/src/main/java/cm/deepdream/vehicleseller/backend/data/VehiclesellerBackendDataApplication.java
@@ -0,0 +1,13 @@
+package cm.deepdream.vehicleseller.backend.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesellerBackendDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesellerBackendDataApplication.class, args);
+	}
+
+}
---
+++ vehicleseller-backend-data/src/test/java/cm/deepdream/vehicleseller/backend/data/VehiclesellerBackendDataApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.deepdream.vehicleseller.backend.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesellerBackendDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
