81e56a70f83ea03d9a8b272ef27a406243263083
deepdream-apps <silvere.djam@gmail.com>
2021-10-03 06:59:24 +0200

    Commit projects

--- vehicleseller-auth-data/src/main/java/cm/deepdream/vehicleseller/model/VehiclesellerAuthDataApplication.java
+++ vehicleseller-auth-data/src/main/java/cm/deepdream/vehicleseller/model/VehiclesellerAuthDataApplication.java
@@ -1,4 +1,4 @@
-package cm.deepdream.vehicleseller.auth.data;
+package cm.deepdream.vehicleseller.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
